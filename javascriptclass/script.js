b732f8ae188a67ab027b16047c35982694a41ef5
kannika-k1 <suruthi927@gmail.com>
2025-11-17 03:34:26 -1000

    sixth commit

---
+++ javascriptclass/script.js
@@ -0,0 +1,82 @@
+// it is scripting plus programming language
+// it is browser based programming language
+//string is the series of words
+
+// console.log("hello my world")
+// console.log(`Hello my world`)
+//window.alert("welccome to javascript")
+// document.getElementById('h1').textContent="welcome to javascript"
+// document.getElementById('p1').textContent="hello javascript"
+
+
+//variables -container store the value 
+// it will bahave as 
+//follow camel rule
+//declare variable and next assign let var const 
+//let global scope
+//var 
+//const not changable
+//${} called placeholder
+
+// let a=19;
+// let b=25;
+// console.log(a+b)
+// b=99
+// console.log(b)
+// let age=24
+// console.log("your age is",age)
+// console.log("your age is"+age)
+// console.log(`you are ${age} years old`)
+
+// let firstname='kannika'
+// console.log(typeof(firstname))
+// let favfood='biriyani'
+// console.log(`my favourite food is ${favfood}`)
+
+// let online=true;
+// console.log(typeof(online));
+
+// let firstname='kannika';
+// let age=23
+// let student=true
+// document.getElementById('h1').innerHTML=`my name is :${firstname}`;
+// document.getElementById('h2').innerHTML=` my age is :${age}`;
+// document.getElementById('h3').innerHTML=` i am a student :${student}`;
+
+
+//arithmetic operation operands and operators add mul div sub
+
+//let student=31;
+// student=student*2;
+// student=student/2;
+// student=student**2;
+// student=student%2;
+// student+=1;
+// student-=1;
+// student*=1;
+// student/=1;
+// student%=2;
+// student++
+// student--
+//student**
+// student++
+//console.log(student);
+//podmas rule
+// B → Brackets  
+// O → Orders (powers, square, square root)  
+// D → Division  
+// M → Multiplication  
+// A → Addition  
+// S → Subtraction
+//  let result=3+4+(6*7)-7*5**2;
+//  console.log(result);
+
+
+
+
+
+
+
+
+
+
